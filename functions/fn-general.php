d6592e423a9d256f02be5fec98ebd0edc5c48ba9
andream-cpu <andrea.m@dadostudio.com>
2026-04-14 12:35:14 +0200

    Modifiche typografia + creazione post type prodotto

--- functions/fn-general.php
+++ functions/fn-general.php
@@ -14,3 +14,42 @@ function fix_webp_mime_type($data, $file, $filename, $mimes) {
     return $data;
 }
 add_filter('wp_check_filetype_and_ext', 'fix_webp_mime_type', 10, 4);
+
+/* Create post type product */
+add_action('init', 'create_product_post_type');
+
+function create_product_post_type() {
+    $labels = array(
+        'name' => _x('Prodotti', 'post type general name'),
+        'singular_name' => _x('Prodotto', 'post type singular name'),
+        'menu_name' => _x('Prodotti', 'admin menu'),
+        'name_admin_bar' => _x('Prodotto', 'add new on admin bar'),
+        'add_new' => _x('Aggiungi Nuovo', 'product'),
+        'add_new_item' => __('Aggiungi Nuovo Prodotto'),
+        'new_item' => __('Nuovo Prodotto'),
+        'edit_item' => __('Modifica Prodotto'),
+        'view_item' => __('Visualizza Prodotto'),
+        'all_items' => __('Tutti i Prodotti'),
+        'search_items' => __('Cerca Prodotti'),
+        'not_found' => __('Nessun prodotto trovato'),
+        'not_found_in_trash' => __('Nessun prodotto trovato nel Cestino'),
+        'parent_item_colon' => '',
+        'menu_icon' => 'dashicons-products',
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'product'),
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => null,
+        'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+    );
+    register_post_type('product', $args);
+}
+?>
---
+++ typography.php
@@ -0,0 +1,178 @@
+<?php
+    /* Template Name: Typography */
+    get_header();
+?>
+    <section>
+        <div class="container sp-py-5">
+            <div class="row">
+                <div class="col-12">
+                    <h0 class="h0">Heading 0</h0>
+                    <h1 class="h1">Heading 1</h1>
+                    <h2 class="h2">Heading 2</h2>
+                    <h3 class="h3">Heading 3</h3>
+                    <h4 class="h4">Heading 4</h4>
+                    <h5 class="h5">Heading 5</h5>
+                    <h6 class="h6">Heading 6</h6>
+                    <h7 class="h7">Heading 7</h7>
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                    <p class="subtitle-1">Subtitle 1</p>
+                    <p class="subtitle-2">Subtitle 2</p>
+                    <p class="subtitle-3">Subtitle 3</p>
+                    <p class="subtitle-4">Subtitle 4</p>
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                    <p class="p-bigger">
+                        Body 1<br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat porttitor fringilla. In quis dapibus nisi. Suspendisse nec diam hendrerit, tincidunt nisl at, suscipit risus. Nulla ut molestie purus. Etiam lobortis, ex sed eleifend mollis, enim ipsum interdum nibh, a porta est urna sit amet ex. Pellentesque scelerisque, odio aliquam mollis scelerisque, massa quam blandit mauris, quis ornare lectus mi sit amet tellus. Maecenas posuere scelerisque nunc, eu porttitor mauris suscipit et. Sed massa sem, maximus sit amet faucibus ut, scelerisque in augue. Proin lacinia lectus risus, in ultrices mauris aliquam sit amet. Nam lacus augue, porttitor facilisis neque id, ultricies malesuada magna. In placerat porttitor mauris, vitae tristique massa lobortis volutpat. Duis in elit varius, lacinia metus sed, hendrerit est. Praesent et velit sagittis, vestibulum leo sit amet, porttitor felis. Fusce eu feugiat neque. Maecenas tincidunt ultrices ex.
+                        Vestibulum leo libero, vulputate quis aliquam ac, scelerisque vel libero. Morbi consectetur sapien quis cursus auctor. Quisque vel tempus odio. Cras accumsan justo ac congue condimentum. Cras venenatis elementum lorem, nec iaculis ipsum dictum in. Quisque nec mi diam. Vestibulum at fringilla risus. Maecenas ac lacus metus. Aliquam vitae nisl vitae erat bibendum cursus.
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin in dolor in mauris tincidunt scelerisque in ac tellus. Ut ac placerat diam, nec interdum leo. In risus velit, congue sit amet mi eget, lacinia hendrerit ligula. In vitae odio ac justo tincidunt pharetra. Pellentesque mauris diam, ultrices a urna vitae, egestas facilisis leo. Nulla arcu turpis, consequat eget sagittis non, convallis egestas arcu.
+                    </p>
+                    <p class="p-big">
+                        Body 2<br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat porttitor fringilla. In quis dapibus nisi. Suspendisse nec diam hendrerit, tincidunt nisl at, suscipit risus. Nulla ut molestie purus. Etiam lobortis, ex sed eleifend mollis, enim ipsum interdum nibh, a porta est urna sit amet ex. Pellentesque scelerisque, odio aliquam mollis scelerisque, massa quam blandit mauris, quis ornare lectus mi sit amet tellus. Maecenas posuere scelerisque nunc, eu porttitor mauris suscipit et. Sed massa sem, maximus sit amet faucibus ut, scelerisque in augue. Proin lacinia lectus risus, in ultrices mauris aliquam sit amet. Nam lacus augue, porttitor facilisis neque id, ultricies malesuada magna. In placerat porttitor mauris, vitae tristique massa lobortis volutpat. Duis in elit varius, lacinia metus sed, hendrerit est. Praesent et velit sagittis, vestibulum leo sit amet, porttitor felis. Fusce eu feugiat neque. Maecenas tincidunt ultrices ex.
+                        Vestibulum leo libero, vulputate quis aliquam ac, scelerisque vel libero. Morbi consectetur sapien quis cursus auctor. Quisque vel tempus odio. Cras accumsan justo ac congue condimentum. Cras venenatis elementum lorem, nec iaculis ipsum dictum in. Quisque nec mi diam. Vestibulum at fringilla risus. Maecenas ac lacus metus. Aliquam vitae nisl vitae erat bibendum cursus.
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin in dolor in mauris tincidunt scelerisque in ac tellus. Ut ac placerat diam, nec interdum leo. In risus velit, congue sit amet mi eget, lacinia hendrerit ligula. In vitae odio ac justo tincidunt pharetra. Pellentesque mauris diam, ultrices a urna vitae, egestas facilisis leo. Nulla arcu turpis, consequat eget sagittis non, convallis egestas arcu.
+                    </p>
+                    <p>
+                        Body 3<br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat porttitor fringilla. In quis dapibus nisi. Suspendisse nec diam hendrerit, tincidunt nisl at, suscipit risus. Nulla ut molestie purus. Etiam lobortis, ex sed eleifend mollis, enim ipsum interdum nibh, a porta est urna sit amet ex. Pellentesque scelerisque, odio aliquam mollis scelerisque, massa quam blandit mauris, quis ornare lectus mi sit amet tellus. Maecenas posuere scelerisque nunc, eu porttitor mauris suscipit et. Sed massa sem, maximus sit amet faucibus ut, scelerisque in augue. Proin lacinia lectus risus, in ultrices mauris aliquam sit amet. Nam lacus augue, porttitor facilisis neque id, ultricies malesuada magna. In placerat porttitor mauris, vitae tristique massa lobortis volutpat. Duis in elit varius, lacinia metus sed, hendrerit est. Praesent et velit sagittis, vestibulum leo sit amet, porttitor felis. Fusce eu feugiat neque. Maecenas tincidunt ultrices ex.
+                        Vestibulum leo libero, vulputate quis aliquam ac, scelerisque vel libero. Morbi consectetur sapien quis cursus auctor. Quisque vel tempus odio. Cras accumsan justo ac congue condimentum. Cras venenatis elementum lorem, nec iaculis ipsum dictum in. Quisque nec mi diam. Vestibulum at fringilla risus. Maecenas ac lacus metus. Aliquam vitae nisl vitae erat bibendum cursus.
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin in dolor in mauris tincidunt scelerisque in ac tellus. Ut ac placerat diam, nec interdum leo. In risus velit, congue sit amet mi eget, lacinia hendrerit ligula. In vitae odio ac justo tincidunt pharetra. Pellentesque mauris diam, ultrices a urna vitae, egestas facilisis leo. Nulla arcu turpis, consequat eget sagittis non, convallis egestas arcu.
+                    </p>
+                    <p class="p-small">
+                        Body 4<br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat porttitor fringilla. In quis dapibus nisi. Suspendisse nec diam hendrerit, tincidunt nisl at, suscipit risus. Nulla ut molestie purus. Etiam lobortis, ex sed eleifend mollis, enim ipsum interdum nibh, a porta est urna sit amet ex. Pellentesque scelerisque, odio aliquam mollis scelerisque, massa quam blandit mauris, quis ornare lectus mi sit amet tellus. Maecenas posuere scelerisque nunc, eu porttitor mauris suscipit et. Sed massa sem, maximus sit amet faucibus ut, scelerisque in augue. Proin lacinia lectus risus, in ultrices mauris aliquam sit amet. Nam lacus augue, porttitor facilisis neque id, ultricies malesuada magna. In placerat porttitor mauris, vitae tristique massa lobortis volutpat. Duis in elit varius, lacinia metus sed, hendrerit est. Praesent et velit sagittis, vestibulum leo sit amet, porttitor felis. Fusce eu feugiat neque. Maecenas tincidunt ultrices ex.
+                        Vestibulum leo libero, vulputate quis aliquam ac, scelerisque vel libero. Morbi consectetur sapien quis cursus auctor. Quisque vel tempus odio. Cras accumsan justo ac congue condimentum. Cras venenatis elementum lorem, nec iaculis ipsum dictum in. Quisque nec mi diam. Vestibulum at fringilla risus. Maecenas ac lacus metus. Aliquam vitae nisl vitae erat bibendum cursus.
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin in dolor in mauris tincidunt scelerisque in ac tellus. Ut ac placerat diam, nec interdum leo. In risus velit, congue sit amet mi eget, lacinia hendrerit ligula. In vitae odio ac justo tincidunt pharetra. Pellentesque mauris diam, ultrices a urna vitae, egestas facilisis leo. Nulla arcu turpis, consequat eget sagittis non, convallis egestas arcu.
+                    </p>
+                    <p class="p-smaller">
+                        Body 5<br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent feugiat porttitor fringilla. In quis dapibus nisi. Suspendisse nec diam hendrerit, tincidunt nisl at, suscipit risus. Nulla ut molestie purus. Etiam lobortis, ex sed eleifend mollis, enim ipsum interdum nibh, a porta est urna sit amet ex. Pellentesque scelerisque, odio aliquam mollis scelerisque, massa quam blandit mauris, quis ornare lectus mi sit amet tellus. Maecenas posuere scelerisque nunc, eu porttitor mauris suscipit et. Sed massa sem, maximus sit amet faucibus ut, scelerisque in augue. Proin lacinia lectus risus, in ultrices mauris aliquam sit amet. Nam lacus augue, porttitor facilisis neque id, ultricies malesuada magna. In placerat porttitor mauris, vitae tristique massa lobortis volutpat. Duis in elit varius, lacinia metus sed, hendrerit est. Praesent et velit sagittis, vestibulum leo sit amet, porttitor felis. Fusce eu feugiat neque. Maecenas tincidunt ultrices ex.
+                        Vestibulum leo libero, vulputate quis aliquam ac, scelerisque vel libero. Morbi consectetur sapien quis cursus auctor. Quisque vel tempus odio. Cras accumsan justo ac congue condimentum. Cras venenatis elementum lorem, nec iaculis ipsum dictum in. Quisque nec mi diam. Vestibulum at fringilla risus. Maecenas ac lacus metus. Aliquam vitae nisl vitae erat bibendum cursus.
+                        Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin in dolor in mauris tincidunt scelerisque in ac tellus. Ut ac placerat diam, nec interdum leo. In risus velit, congue sit amet mi eget, lacinia hendrerit ligula. In vitae odio ac justo tincidunt pharetra. Pellentesque mauris diam, ultrices a urna vitae, egestas facilisis leo. Nulla arcu turpis, consequat eget sagittis non, convallis egestas arcu.
+                    </p>
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                    <div class="table-1 mt-5">
+                        Table 1<br>
+                        <table class="w-100 mt-5">
+                            <tr>
+                                <th>Column 1</th>
+                                <th>Column 2</th>
+                                <th>Column 3</th>
+                            </tr>
+                            <tr>
+                                <td>Cell 1</td>
+                                <td>Cell 2</td>
+                                <td>Cell 3</td>
+                            </tr>
+                            <tr>
+                                <td>Cell 4</td>
+                                <td>Cell 5</td>
+                                <td>Cell 6</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="table-2 mt-5">
+                        Table 2<br>
+                        <table class="w-100 mt-5">
+                            <tr>
+                                <th>Column 1</th>
+                                <th>Column 2</th>
+                                <th>Column 3</th>
+                            </tr>
+                            <tr>
+                                <td>Cell 1</td>
+                                <td>Cell 2</td>
+                                <td>Cell 3</td>
+                            </tr>
+                            <tr>
+                                <td>Cell 4</td>
+                                <td>Cell 5</td>
+                                <td>Cell 6</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="table-3 mt-5">
+                        Table 3<br>
+                        <table class="w-100 mt-5">
+                            <tr>
+                                <th>Column 1</th>
+                                <th>Column 2</th>
+                                <th>Column 3</th>
+                            </tr>
+                            <tr>
+                                <td>Cell 1</td>
+                                <td>Cell 2</td>
+                                <td>Cell 3</td>
+                            </tr>
+                            <tr>
+                                <td>Cell 4</td>
+                                <td>Cell 5</td>
+                                <td>Cell 6</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="table-4 mt-5">
+                        Table 4<br>
+                        <table class="w-100 mt-5">
+                            <tr>
+                                <th>Column 1</th>
+                                <th>Column 2</th>
+                                <th>Column 3</th>
+                            </tr>
+                            <tr>
+                                <td>Cell 1</td>
+                                <td>Cell 2</td>
+                                <td>Cell 3</td>
+                            </tr>
+                            <tr>
+                                <td>Cell 4</td>
+                                <td>Cell 5</td>
+                                <td>Cell 6</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                    <a href="#" class="btn btn-primary"><span>Primary Button</span></a>
+                    <a href="#" class="btn btn-secondary"><span>Secondary Button</span></a>
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                    <div class="number-1 mt-5">
+                        Number 1<br>
+                        <p class="number-1">01</p>
+                    </div>
+                    <div class="number-2 mt-5">
+                        Number 2<br>
+                        <p class="number-2">02</p>
+                    </div>
+                    <div class="number-3 mt-5">
+                        Number 3<br>
+                        <p class="number-3">03</p>
+                    </div>
+                </div>
+                <hr class="my-4">
+                <div class="col-12">
+                    <div class="product-1 mt-5">
+                        <p class="product-1">Product 1</p>
+                    </div>
+                    <div class="product-2 mt-5">
+                        <p class="product-2">Product 2</p>
+                    </div>
+                    <div class="product-3 mt-5">
+                        <p class="product-3">Product 3</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php
+    get_footer();
+?>
