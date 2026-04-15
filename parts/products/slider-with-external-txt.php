9edbb5bc7e9c68dda02317e0ab3161d51370907a
andream-cpu <andrea.m@dadostudio.com>
2026-04-15 17:20:05 +0200

    Commit per sezione dettagli

---
+++ parts/products/slider-with-external-txt.php
@@ -0,0 +1,53 @@
+<section class="section-slider-text-ext">
+    <div class="container-fluid position-relative">
+        <div class="row">
+            <div class="col-8">
+                <div class="single-slider-carousel-txt-ext owl-carousel">
+                    <div class="slider-item">
+                        <figure class="sp-mb-0 rounded-3 overflow-hidden position-relative">
+                            <img src="<?php echo wp_get_upload_dir()['baseurl']; ?>/2026/04/slider-1-705x510.png" alt="Related Product 1" class="w-100">
+                        </figure>
+                    </div>
+                    <div class="slider-item">
+                        <figure class="sp-mb-0 rounded-3 overflow-hidden position-relative">
+                            <img src="<?php echo wp_get_upload_dir()['baseurl']; ?>/2026/04/slider-2-700x526.png" alt="Related Product 3" class="w-100">
+                        </figure>
+                    </div>
+                    <div class="slider-item">
+                        <figure class="sp-mb-0 rounded-3 overflow-hidden position-relative">
+                            <img src="<?php echo wp_get_upload_dir()['baseurl']; ?>/2026/04/slider-3-700x526.png" alt="Related Product 2" class="w-100">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="position-relative sp-py-4 sp-md-py-12 sp-lg-py-5">
+                    <div class="text-slide-0 active w-100">
+                        <div class="ts-title h7 fw-light">Distanza minima di lavoro</div>
+                        <div class="fw-light sp-mt-2 sp-md-mt-4">30,5 cm</div>
+                    </div>
+                    <div class="text-slide-1 w-100">
+                        <div class="ts-title h7 fw-light">Montaggio</div>
+                        <div class="fw-light sp-mt-2 sp-md-mt-4">A = 3,2 m misura da centro canalina a centro ponte. Indicativamente per auto lunghe da 2,7 a 5,10 m</div>
+                    </div>
+                    <div class="text-slide-2 w-100">
+                        <div class="ts-title h7 fw-light">Dettaglio componenti</div>
+                        <div class="fw-light sp-mt-2 sp-md-mt-4">
+                            <ol>
+                                <li>Carrello</li>
+                                <li>Tubo fisso di AL anodizzato</li>
+                                <li>Tubo telescopico di AL anodizzato</li>
+                                <li>Maniglia di movimentazione</li>
+                                <li>Molla a gas a trazione bloccabile</li>
+                                <li>Tubo in AL anodizzato</li>
+                                <li>Snodo con tubo flessibile</li>
+                                <li>Maniglia di sblocco</li>
+                                <li>Bocchetta</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
