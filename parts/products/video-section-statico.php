0499d399ec8c5fc3cc16c7cf47f3ddfbecc478a3
Gigi1982 <basile.luigi@gmail.com>
2026-04-21 09:56:33 +0200

    Video modificato

--- parts/products/video-section-statico.php
+++ parts/products/video-section-statico.php
@@ -4,7 +4,7 @@
             <div class="col-lg-10 offset-lg-1 col-12">
                 <figure class="respimg sp-mb-0 aspect-ratio-16x7 rounded-3 overflow-hidden position-relative">
                     <video>
-                        <source src="https://flcsite.dadostudio.com/wp-content/uploads/2026/04/mov_bbb.mp4" type="video/mp4">
+                        <source src="https://flcsite.dadostudio.com/wp-content/uploads/2026/04/232065142-aerial-view-fun-fair-twilight-.mp4" type="video/mp4">
                         Your browser does not support the video tag.
                     </video>
                     <div class="video-overlay position-absolute w-100 h-100 top-0 left-0 d-flex align-items-center justify-content-center">
